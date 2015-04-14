8cb626404a80e34c4c8e997690c80d93451daf0a
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2015-04-14 20:42:43 +0300

    add ut for parseFlag

--- test.js
+++ test.js
@@ -15,8 +15,17 @@ process.argv.forEach(function (val, index, array) {
 });
 
 /* declaration of modules  */
+var assert = require('assert');
 var ctfile = require("./ctfile");
-
-console.log(ctfile.getVersion());
-
-
+console.log("Test ctfile.getVersion method(Test for deep equality)");
+assert.deepStrictEqual({ moduleVersion: '0.0.0' }, ctfile.getVersion());
+console.log("Test internal function parseFlag");
+assert.equal(true, ctfile.ut_parseFlag(1));
+assert.equal(true, ctfile.ut_parseFlag("  1"));
+assert.equal(true, ctfile.ut_parseFlag(true));
+assert.equal(true, ctfile.ut_parseFlag("  1 "));
+assert.equal(false, ctfile.ut_parseFlag(false));
+assert.equal(false, ctfile.ut_parseFlag(0));
+assert.equal(false, ctfile.ut_parseFlag(" 0"));
+assert.equal(false, ctfile.ut_parseFlag(" 0 0"));
+assert.equal(false, ctfile.ut_parseFlag(" 1 0"));
