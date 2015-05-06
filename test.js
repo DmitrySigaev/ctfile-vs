5cf72f7bbd63decb6f91242edee50753029740fb
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2015-05-06 18:38:48 +0300

    UT of internal cleanInvChars function

--- test.js
+++ test.js
@@ -97,5 +97,20 @@ assert.equal('@ACETYLCHOLINE;&;stuff@', ctfile.ut_cleanWSChs2('\n\n\nACETYLCHOLI
 assert.equal('@ACETYLCHOLINE & stuff@', ctfile.ut_cleanWSChs2('\n\n\nACETYLCHOLINE & stuff\n\n\n', ' '));
 assert.equal('@ACETYLCHOLINE;&;stuff@', ctfile.ut_cleanWSChs2('\n\n\r\nACETYLCHOLINE & stuff\n\r\n\n', ';'));
 assert.equal('@ACETYLCHOLINE;&;stuff@GTMACCS-II11299515322D;1;0.00377;0.00000;0;GST', ctfile.ut_cleanWSChs2('\n\n\nACETYLCHOLINE & stuff\n\n\nGTMACCS-II11299515322D 1   0.00377     0.00000     0    GST', ';'));
-assert.equal('@ACETYLCHOLINE;&;stuff@GTMACCS-II11299515322D;1;0.00377;0.00000;0;GST', ctfile.ut_cleanWSChs2('\n\n\nACETYLCHOLINE & stuff\n\n\r\nGTMACCS-II11299515322D 1   0.00377     0.00000     0    GST', ';'));
+assert.equal('@ACETYLCHOLINE;&;stuff@GTMACCS-II11299515322D;1;0.00377;0.00000;0;GST', ctfile.ut_cleanWSChs2('\n\n\nACETYLCHOLINE & stuff\n\n\r\nGTMACCS-II11299515322D 1   0.00377  \r   0.00000     0    GST', ';'));
 
+
+console.log("Test internal function ut_cleanInvChars");
+console.log("should compare with patterns");
+assert.equal('\nACETYLCHOLINE & stuff\n   \n   \n', ctfile.ut_cleanInvChars('\n\r\f\v\tACETYLCHOLINE & stuff\n  \r\f\v \t\n \u00a0\u00a0  \n', ';'));
+
+
+var fs = require('fs');
+var file_data = fs.readFileSync('./ctfile/data-in/accholine.mol', 'utf8');
+var inx = file_data.search(/V[23]000/gi);
+var subs = file_data.substring(0, inx+5);
+var test2 = ctfile.ut_cleanWSChs2(subs);
+var test3 = ctfile.ut_cleanInvChars(subs);
+
+var test3 = ctfile.ut_cleanInvChars(subs);
+var test2n = ctfile.ut_cleanWSChs2(test3);
